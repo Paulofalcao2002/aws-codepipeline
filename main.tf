8bedf867055925c832ffcbc822b3a50bdd5c0254
Paulofalcao2002 <paulohenriquemoreirafalcao@gmail.com>
2023-05-10 10:41:09 -0300

    refactor: Abstracts bootstrap module

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ provider "aws" {
     shared_credentials_files = ["C:/Users/paulo/.aws/credentials"]
 }
 
-## Build an S3 bucket for logging
-module "bootstrap" {
-  source                              = "./modules/bootstrap"
-  s3_logging_bucket_name              = "paulo-codebuild-demo-logging-bucket"
-  codebuild_iam_role_name             = "CodeBuildIamRole"
-  codebuild_iam_role_policy_name      = "CodeBuildIamRolePolicy"
-  terraform_codecommit_repo_arn       = module.codecommit.terraform_codecommit_repo_arn
-  codepipeline_artifact_bucket_arn   = module.codepipeline.codepipeline_artifact_bucket_arn
-}
-
 ## Build a CodeCommit git repo
 module "codecommit" {
     source = "./modules/codecommit"
@@ -24,9 +14,11 @@ module "codecommit" {
 module "codebuild" {
   source                                 = "./modules/codebuild"
   codebuild_project_test_name            = "CodeBuildTestProject"
-  s3_logging_bucket_id                   = module.bootstrap.s3_logging_bucket_id
-  codebuild_iam_role_arn                 = module.bootstrap.codebuild_iam_role_arn
-  s3_logging_bucket                      = module.bootstrap.s3_logging_bucket
+  s3_logging_bucket_name                 = "paulo-codebuild-demo-logging-bucket"
+  codebuild_iam_role_name                = "CodeBuildIamRole"
+  codebuild_iam_role_policy_name         = "CodeBuildIamRolePolicy"
+  terraform_codecommit_repo_arn          = module.codecommit.terraform_codecommit_repo_arn
+  codepipeline_artifact_bucket_arn       = module.codepipeline.codepipeline_artifact_bucket_arn
 }
 
 ## Build a CodePipeline
--- modules/codebuild/codebuild_variables.tf
+++ modules/codebuild/codebuild_variables.tf
@@ -2,14 +2,22 @@ variable "codebuild_project_test_name" {
   description = "Name for test CodeBuild Project"
 }
 
-variable "s3_logging_bucket_id" {
-  description = "ID of the S3 bucket for access logging"
+variable "s3_logging_bucket_name" {
+  description = "Name of S3 bucket to use for access logging"
 }
 
-variable "s3_logging_bucket" {
-  description = "Name of the S3 bucket for access logging"
+variable "codebuild_iam_role_name" {
+  description = "Name for IAM Role utilized by CodeBuild"
 }
 
-variable "codebuild_iam_role_arn" {
-  description = "ARN of the CodeBuild IAM role"
+variable "codebuild_iam_role_policy_name" {
+  description = "Name for IAM policy used by CodeBuild"
+}
+
+variable "terraform_codecommit_repo_arn" {
+  description = "Terraform CodeCommit git repo ARN"
+}
+
+variable "codepipeline_artifact_bucket_arn" {
+  description = "CodePiepline artifact bucket ARN"
 }
